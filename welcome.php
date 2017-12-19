d0bce87409cb8fe4d4f916bb012dcdb9bb476fc7
emmadrews <drews.emma@gmail.com>
2017-12-19 16:02:00 -0600

    emma form test

--- welcome.php
+++ welcome.php
@@ -5,32 +5,17 @@ $public_api_key = "13ce4d0e43aa22f28c9c";
 $private_api_key = "fc36793623f9bee1ae46";
 
 // Form variable(s)
-$email = $_POST['email'];
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$groups = array(123456, 654321);
-
-// Member data other than email should be passed in an array called "fields"
-$member_data = array(
-    "email" => $email,
-    "fields" => array(
-        "first_name" => $first_name,
-        "last_name" => $last_name
-    ),
-    "group_ids" => $groups
-);
+$email = 'me@test.com';
 
 // Set URL
-$url = "https://api.e2ma.net/".$account_id."/members/add";
+$url = "https://api.e2ma.net/" . $account_id . "/members/email/" . $email;
 
 // Open connection
 $ch = curl_init();
 
-// Set the url, number of POST vars, POST data
+// Make the curl call
 curl_setopt($ch, CURLOPT_USERPWD, $public_api_key . ":" . $private_api_key);
 curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST, count($member_data));
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($member_data));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -38,14 +23,12 @@ $head = curl_exec($ch);
 $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
-//execute post
+// check for errors
 if($http_code > 200) {
-    $app_message = "Error sending subscription request";
+    print "Error getting member:\r\n";
     print_r($head);
 } else {
+    print "Member information:\r\n";
     print_r($head);
-    $app_message = "Success!";
 }
-
-echo $app_message;
 ?>
